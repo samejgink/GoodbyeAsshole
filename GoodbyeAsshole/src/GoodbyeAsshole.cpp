866af9417b579563197999f6277e943849cafc33
Jimmy King <samejgink@gmail.com>
2016-06-18 08:28:16 -0500

    First commit

---
+++ GoodbyeAsshole/src/GoodbyeAsshole.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : GoodbyeAsshole.cpp
+// Author      : Jimmy King
+// Version     :
+// Copyright   : Free to use
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Goodbye, asshole!" << endl;
+	return 0;
+}
